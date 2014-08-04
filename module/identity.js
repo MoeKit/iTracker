07ee0df2283681e762fc89db1685668eccc70960
airyland <airyland@qq.com>
2014-08-04 20:27:05 +0800

    add identify function

--- module/identity.js
+++ module/identity.js
@@ -1,1 +1,9 @@
 // 与uuid联结的uid信息
+var beacon = require('../lib/beacon');
+module.exports = function (uid, uuid) {
+    beacon({
+        event: 'identify',
+        uuid: uuid,
+        uid: uid
+    });
+};
