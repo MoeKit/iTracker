8f95faca3e60d68701a272c61253cbd7d8eaf990
airyland <airyland@qq.com>
2014-08-06 16:27:47 +0800

    m站不取微博id,后面再看看有没有jsonp接口

--- module/sina_uid.js
+++ module/sina_uid.js
@@ -1,25 +1,23 @@
 // 获取新浪微博uid
-var url ='https://api.weibo.com/2/account/get_uid.json?source=2456743075&';
+var url = 'https://api.weibo.com/2/account/get_uid.json?source=2456743075&';
 var beacon = require('../lib/beacon');
 var uid = require('../lib/uid');
 var store = require('local-store');
 var jsonp = require('../lib/jsonp');
-module.exports = function(uuid) {
-    if (!store.get('sina')) {
-      jsonp(url,{},'callback',function(data){
-        if (data.code===1) {
+module.exports = function (uuid) {
+    if (!store.get('sina') && document.location.href.indexOf('m.seedit.com') === -1) {
+        jsonp(url, {}, 'callback', function (data) {
+            if (data.code === 1) {
                 beacon({
                     event: 'sina',
                     uuid: uuid,
-                    sid:data.data.uid
-                }, function() {
+                    sid: data.data.uid
+                }, function () {
                     store.set('sina', true);
                 });
             } else {
                 window.console && console.log('no sina id ');
             }
-      });
-            
-       
+        });
     }
 };
